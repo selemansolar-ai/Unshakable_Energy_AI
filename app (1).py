346f1dd5e48b03c011f635ac3de86572e2efa769
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-27 02:47:31 +0300

    Fix syntax error in set_page_config call

--- app (1).py
+++ app (1).py
@@ -1,7 +1,7 @@
 
 import requests
 import streamlit as st
-st.set_page_config(page_title="Unshakable Energy", layout='wide'
+st.set_page_config(page_title="Unshakable Energy", layout='wide')
 st.title("☀️ UNSHAKABLE ENERGY - AI SOLAR PREDICTION")
 st.success(" App iko live✅")
 st.subheader(f"⛅ Hali ya Hewa Mwanza - saa 24 zijazo")
