11fd85f2d0393b7254bfd37d62a95ad2c355e375
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-27 15:27:34 +0300

    Fix import statement for streamlit_folium

--- app (1).py
+++ app (1).py
@@ -5,7 +5,7 @@ from datetime import datetime
 import time
 import sqlite3
 import folium
-from streamlit_folium import folium
+from streamlit_folium import st_folium
 
 API_KEY = st.secrets["API_KEY"]
 CITY = "Mwanza, TZ"
