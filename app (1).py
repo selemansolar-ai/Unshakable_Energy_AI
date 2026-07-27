54223633abe61ab184b66917eb34ed3708d54cdb
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-27 22:06:11 +0300

    Remove caching from save_data function
    Removed caching decorator from save_data function.

--- app (1).py
+++ app (1).py
@@ -18,7 +18,6 @@ conn = sqlite3.connect('solar_data.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS history(time TEXT, temp REAl, cloud INTEGER, prediction TEXT)''')
 
-@st.cache_data
 def save_data(time, temp, cloud, pred):
   conn = sqlite3.connect('solar_data.db')
   c = conn.cursor()
