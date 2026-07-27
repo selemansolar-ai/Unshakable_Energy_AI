cfee1687c36c5719a9bf5f20224e1f9362aa740e
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-27 03:00:04 +0300

    update

--- app (1).py
+++ app (1).py
@@ -1,7 +1,35 @@
 
 import requests
 import streamlit as st
-st.set_page_config(page_title="Unshakable Energy", layout='wide')
-st.title("☀️ UNSHAKABLE ENERGY - AI SOLAR PREDICTION")
-st.success(" App iko live✅")
-st.subheader(f"⛅ Hali ya Hewa Mwanza - saa 24 zijazo")
+from datetime import datetime
+import time, json
+
+
+API_KEY = "13b94b58771cd53150cf1bf4c1fd3f32"
+CITY = "Mwanza, TZ"
+
+def get_weather():
+  url=f"https://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={API-kEY}&units=metric"
+  response = requests.get(url)
+  data = response.json()
+
+  st.title("☀️ UNSHAKABLE ENERGY - AI SOLAR PREDICTION")
+  st.subheader(f"⛅ Hali ya Hewa Mwanza - saa 24 zijazo")
+  for item in data['list'][:8]: # saa 8 = 24 saa
+    time = datatime.fromtimestamp(item['dt']).strftime('%d/%m %H:%M')
+    temp = item['main']['temp']
+    cloud = item['clouds']['all'] # % ya mawingu ndio muhimu kwa jua
+    weather = item['weather'][0]['description']
+
+    col1,col2,col3 = st.columns(3)
+    col1.write(f"**{time}**")
+    col2.write(f"{weather} | {temp}°C")
+    col3.write(f"Mawingu: {cloud}%")
+
+    if cloud < 20:
+      st.success("☀️ PREDICTION: Jua kali - power itakuwa JUU ")
+    elif cloud < 60:
+      st.warning("⛅ PREDICTIO:Jua la kawaida- Power ya kati, dhibiti matumizi")
+    else:
+      st.error("☁️ PREDICTION: Mawingu meng - Power itakuwa chini ,chaji betri leo")
+    st.divider()
