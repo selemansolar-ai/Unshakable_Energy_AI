851ed0a65ff8ad06bd96eb974d8f5f73a457c8e5
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-27 22:05:05 +0300

    Implement save_data function for database insertion
    Add save_data function to insert records into history table

--- app (1).py
+++ app (1).py
@@ -20,6 +20,8 @@ c.execute('''CREATE TABLE IF NOT EXISTS history(time TEXT, temp REAl, cloud INTE
 
 @st.cache_data
 def save_data(time, temp, cloud, pred):
+  conn = sqlite3.connect('solar_data.db')
+  c = conn.cursor()
   c.execute("INSERT  INTO history VALUES(?, ?, ?, ?)",(time, temp, cloud, pred))
   conn.commit()
   conn.close()
