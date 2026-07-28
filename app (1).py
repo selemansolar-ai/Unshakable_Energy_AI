0ecddfcaa7bd03ae2ad66b468ac3e913d2f488f8
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-28 06:54:22 +0300

    Refactor prediction logic for day and night

--- app (1).py
+++ app (1).py
@@ -58,26 +58,26 @@ if 'show_times' not in st.session_state:
     hour = int(time.split()[1].split(':')[0])
     is_day = 6<= hour<18
            
-   if is_day:
-     power = 100 - cloud
-     if power > 80:
-       pred = f"Power JUU - {power}%  - Jua kali"
-       st.success(f"☀️ PREDICTION: {pred}")
-     elif power > 50:
-       pred = f"Power ya KATI - {power}% - Mawingu kidogo"
-       st.warning(f"⛅ PREDICTION: {pred}")
-     else:
-       pred = f"Power CHINI {power}%- Mawingu mengi"
-       st.error(f"☁️ PREDICTION: {pred}")
-   else:
-     power = 0 #usiku hakuna jua
-     pred = f"Power SIFURI - Ni Usiku"
-     st.info(f"🌙 PREDICTION: {pred}")
+    if is_day:
+      power = 100 - cloud
+      if power > 80:
+        pred = f"Power JUU - {power}%  - Jua kali"
+        st.success(f"☀️ PREDICTION: {pred}")
+      elif power > 50:
+        pred = f"Power ya KATI - {power}% - Mawingu kidogo"
+        st.warning(f"⛅ PREDICTION: {pred}")
+      else:
+        pred = f"Power CHINI {power}%- Mawingu mengi"
+        st.error(f"☁️ PREDICTION: {pred}")
+    else:
+      power = 0 #usiku hakuna jua
+      pred = f"Power SIFURI - Ni Usiku"
+      st.info(f"🌙 PREDICTION: {pred}")
   
-     times.append(time)
-     temps.append(temp)
-     clouds.append(cloud)
-     preds.append(pred)
+      times.append(time)
+      temps.append(temp)
+      clouds.append(cloud)
+      preds.append(pred)
 
 if time not in st.session_state.shown_times:
   save_data(time, temp, cloud, pred)
