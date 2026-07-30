80123bcaca94a8eee2addf1b009c4ba8ca5e800c
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-07-30 05:46:28 +0300

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -48,8 +48,8 @@ if data and 'list' in data:
   times, temps, clouds, preds = [], [], [], []
   st.subheader(f"⛅ Hali ya Hewa Mwanza - saa 24 zijazo")
 
-if 'show_times' not in st.session_state:
-  st.session_state.shown_times = []
+  if 'show_times' not in st.session_state:
+    st.session_state.shown_times = []
   for item in data['list'][:8]: # saa 8 = Masaa 24
     time = datetime.fromtimestamp(item['dt']).strftime('%d/%m %H:%M')
     temp = item['main']['temp']
